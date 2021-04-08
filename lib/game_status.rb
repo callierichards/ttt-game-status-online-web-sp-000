3cc0a45ab9243da0fa3c2951b6497651814e7c47
callie rogers <callie1013@gmail.com>
2021-04-08 03:40:23 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -27,6 +27,9 @@ def won? (board)
   position_3 = board[win_index_3]
   #value of board at win_index_3
   position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
+end
+end
+
 
 def full? (board)
   board.all? {|i| i == "X" || i == "O"}
